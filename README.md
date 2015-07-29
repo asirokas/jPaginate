d0de036f44fc2eb04cac522647bbb38e4f980b8f
Athanasios Sirokas <asirokas@gmail.com>
2015-07-29 10:56:45 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+#jPaginate
+This is a jQuery plugin for paginating contnet in an html page. It's simple and very easy to use.
+
+##Usage
+1. Load the jQuery libray
+```
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.min.js"></script>
+```
+2. Load the script in the page in which you want to use it.
+```
+    <script type="text/javascript" src="jquery.jPaginate.js"></script>
+```
+3. Use the following syntax with the element that you want to paginate
+```javascript
+	$('#element').jpaginate();
+```
+
+##Options
+The plugin can be called with the following options:
+```
+	pagination_class: "pagination",
+	items_per_page: 5
+```
---
+++ src/jQuery.paginate.js
@@ -0,0 +1,54 @@
+(function($) {
+	$.fn.paginate = function(options) {
+		var defaults = {
+			pagination_class: "pagination",
+			items_per_page: 5
+		};
+
+		// Merge deafults into options
+		var options = $.extend(defaults, options);
+
+		obj = $(this);
+
+		// Count entries in block for pagination
+		var n = obj.children().size();
+
+		// Calculate number of pages
+		var pages = Math.ceil( n / options.items_per_page );
+
+		// Add a div after the #slideshow to put the navigation controls in
+		// TODO: Add class for navigation controls div
+		// TODO: The div for navigation controls should be inside the pagination element so as to have the same width.
+		obj.append(
+			$('<div/>').addClass(options.pagination_class + "__controls").append(
+			$('<ul/>').addClass(options.pagination_class))
+		);
+
+		function createPaginationControls(pages) {
+			// For each div (slide) add a link in span for controls
+			for ( var i = 0; i < pages; i++ ) {
+				$("." + options.pagination_class).append( '<li><a href="#' + (i+1) + '">' + (i+1) + '</a></li>');
+			};
+		}
+
+		function showPage(page_number){
+			var start_from = (page_number * options.items_per_page);
+			var end_on = ((page_number + 1) * options.items_per_page);
+			obj.children().not('.pagination__controls').css('display', 'none').slice(start_from, end_on).css('display','block');
+		}
+
+		createPaginationControls(pages);
+		showPage(0);
+
+		// Navigate to the coresponding slide when clicking on a nav-control
+		$('.pagination li').click(function() {
+			var pageIndex = $(this).index() - 1;
+			// remove active class from all elements
+			$(this).parent().children().removeClass('active');
+			$(this).addClass('active');
+			showPage(pageIndex);
+		});
+
+
+	};
+})(jQuery);
